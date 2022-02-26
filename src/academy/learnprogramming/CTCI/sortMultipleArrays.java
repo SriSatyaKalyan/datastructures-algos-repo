a44b482f0b2587b3fae34cccefa50ef34f7c0731
Satya Kalyan <satyakalyan5@gmail.com>
2022-02-26 15:16:02 -0700

    Finally figured out sortMultipleArrays function

--- src/academy/learnprogramming/CTCI/sortMultipleArrays.java
+++ src/academy/learnprogramming/CTCI/sortMultipleArrays.java
@@ -3,9 +3,9 @@ package academy.learnprogramming.CTCI;
 public class sortMultipleArrays {
 
     public static void main(String[] args) {
-        int k = 3;
+        int k = 4;
         int n = 4;
-        int arr[][] = { {1, 3, 5, 7}, {2, 4, 6, 8}, {0, 9, 10, 11}} ;
+        int arr[][] = { {1, 3, 5, 7}, {2, 4, 6, 8}, {9, 13, 18, 25}, {0, 10, 15, 17}} ;
         sortAll(arr, k, n);
     }
 
@@ -24,17 +24,14 @@ public class sortMultipleArrays {
                     tempArray[n++] = finalArray[x] <= arr[selectedArray][y] ? finalArray[x++] : arr[selectedArray][y++];
                 }
                 if(x < finalArray.length){
-                    System.arraycopy(tempArray, n, finalArray, x, finalArray.length - x);
+                    System.arraycopy(finalArray, x, tempArray, n, finalArray.length - x);
                 }
                 if(y < (arr[selectedArray].length)){
-                    System.arraycopy(tempArray, n, arr[selectedArray], y, arr[selectedArray].length - y);
+                    System.arraycopy(arr[selectedArray], y, tempArray, n, arr[selectedArray].length - y);
                 }
                 finalArray = new int[sizeOfArrays*(selectedArray+1)];
                 System.arraycopy(tempArray, 0, finalArray, 0, tempArray.length);
             }
-
-            //3. Copy tempArray into finalArray
-            //Repeat steps 2 and 3 until end
         }
 
         for(int j = 0; j < finalArray.length; j++){
@@ -43,11 +40,3 @@ public class sortMultipleArrays {
         return finalArray;
     }
 }
-
-//OPTIMIZATION is that if there are elements remaining in the left array, we have to copy them into the tempArray
-//But if there are elements remaining in the right array, we need NOT copy them into the -
-////tempArray as they are already in their designated places
-//        System.arraycopy(arr, i, arr, start+tempIndex, mid-i);
-//
-//                //This action is to copy everything in the tempArray into arr
-//                System.arraycopy(tempArray, 0, arr, start, tempIndex);
