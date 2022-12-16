451db21f648ecaea8894e8093a6740577c425be2
Satya Kalyan <satyakalyan5@gmail.com>
2022-12-16 16:16:34 -0700

    Added 14. Longest Common Prefix to the leetCode folder

---
+++ src/academy/learnprogramming/leetCode/_014longestCommonPrefix.java
@@ -0,0 +1,24 @@
+package academy.learnprogramming.leetCode;
+
+public class _014longestCommonPrefix {
+
+    public static void main(String[] args) {
+        String[] strs = new String[]{"flower","flow","flight"};
+//        System.out.println("flower".indexOf(""));
+        System.out.println("The longest common prefix is: " + longestCommonPrefix(strs));
+    }
+
+    private static String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) { return ""; }
+
+        String prefix = strs[0];
+        for(int i=1; i < strs.length; i++){
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if(prefix.isEmpty()) { return ""; }
+            }
+        }
+
+        return prefix;
+    }
+}
